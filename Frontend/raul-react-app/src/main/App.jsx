71c8aa7c7acf823b7dba1b57cc446f957604d859
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2020-08-17 23:52:19 +0100

    Added dependencies

---
+++ Frontend/raul-react-app/src/main/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import React from 'react'
+import { HashRouter } from 'react-router-dom'
+
+
+export default props => 
+    <HashRouter>
+        <div className="App">
+            Welcome to Raúl
+        </div>
+    </HashRouter>
