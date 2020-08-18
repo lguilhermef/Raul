d1d63c74bfcbb65930246da0697a938c2668d143
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2020-08-18 19:59:54 +0100

    Architecture Improvements

--- Backend/raul-api/raul/Models/League.cs
+++ Backend/raul-api/raul/Models/League.cs
@@ -8,19 +8,46 @@ namespace RaulWebApi.Models
 {
     public class League : Competition
     {
+        private RaulDbContext dbContext;
 
         public League ()
         {
             NvcName = "League";
             NvcDescription = "The most important competition of the Season.";
-            NvcRules = "Should refer: Points per victory, number of games, qualifiers or/and regular, pots used, value in points, etc.";
-
+            RulesId = 1;
         }
 
-        public void generateCalendar ()
+        public List<Game> generateCalendar ()
         {
             
         }
+        
+        public void getCurrentCalendar ()
+        {
 
+            List<Game> currentCalendar = isCurrentLeagueOver() ? getCurrentCalendarFromDb() : generateCalendar();
+            /*if (isCurrentLeagueOver())
+            {
+                List<Game> currentCalendar = generateCalendar();
+            }*/
+        }
+
+        public List<Game> getCurrentCalendarFromDb()
+        {
+            String csvCurrentLeagueGames = dbContext.Competition.OrderBy(c => c.Edition).Where(c => c.NvcName == "League").FirstOrDefault().CsvGame;
+            List<Game> currentCalendar = dbContext.Game
+        }
+
+        public bool isCurrentLeagueOver()
+        {
+            Competition currentLeague = dbContext.Competition.OrderBy(c => c.Edition).Where(c=> c.NvcName == "League").FirstOrDefault();
+
+            if (currentLeague == null)
+            {
+                return true;
+            }
+
+            return currentLeague.WinnerId != null;
+        }
     }
 }
