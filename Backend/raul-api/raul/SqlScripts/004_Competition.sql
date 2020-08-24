306111ffdb4405eb947c9671b9cc0625e758bb1e
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2020-08-24 23:05:55 +0100

    Refactor DB names

--- Backend/raul-api/raul/SqlScripts/004_Competition.sql
+++ Backend/raul-api/raul/SqlScripts/004_Competition.sql
@@ -4,12 +4,12 @@ GO
 CREATE TABLE Competition (
 
 	universe_id int NOT NULL FOREIGN KEY REFERENCES Universe(id),
-	official_name nvarchar(100) NOT NULL,
+	compt_name nvarchar(100) NOT NULL,
 	alias nvarchar(100),
 	history nvarchar(max),
 	rules nvarchar (max),
 	winner_id int FOREIGN KEY REFERENCES RaulUser(username),
 	edition int NOT NULL,
 	is_official_competition bit NOT NULL,
-	PRIMARY KEY (universe_id, official_name, edition)
+	PRIMARY KEY (universe_id, compt_name, edition)
 	);
--- Backend/raul-api/raul/SqlScripts/006_Team.sql
+++ Backend/raul-api/raul/SqlScripts/006_Team.sql
@@ -3,7 +3,7 @@ GO
 
 CREATE TABLE Team (
 
-	official_name nvarchar(100) PRIMARY KEY,
+	team_name nvarchar(100) PRIMARY KEY,
 	alias nvarchar(100) NOT NULL UNIQUE,
 	initials nvarchar(10) NOT NULL UNIQUE,
 	is_national_team bit NOT NULL
