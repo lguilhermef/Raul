e3fe5c6f6918b2843ba4da6e3d9205cdb2a8753f
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2020-08-25 12:53:09 +0100

    Database: Team alias row is now called supporters

--- Backend/raul-api/raul/SqlScripts/006_Team.sql
+++ Backend/raul-api/raul/SqlScripts/006_Team.sql
@@ -5,7 +5,7 @@ CREATE TABLE Team (
 
 	team_name nvarchar(100) PRIMARY KEY,
 	country nvarchar(50) FOREIGN KEY REFERENCES Country(country_name),
-	alias nvarchar(100) NOT NULL UNIQUE,
+	supporters nvarchar(100) NOT NULL UNIQUE,
 	initials nvarchar(10) NOT NULL UNIQUE,
 	is_national_team bit NOT NULL
 	);
